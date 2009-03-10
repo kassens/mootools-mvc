029c5b302f56d962ed0c9e8a33d08dec22de92b0
Jan Kassens <jan@kassens.net>
2009-03-10 19:08:53 +0100

    adding a Controller class

--- Demos/converter/converter.js
+++ Demos/converter/converter.js
@@ -2,37 +2,47 @@
 // = Views =
 // =========
 var MyInputView = View.create({
+
 	html:
 		'<div class="input">' +
 			'<label for="no-id">untitled</label>' +
 			'<input type="text" id="no-id" value=""/>' +
 		'</div>',
+
 	outlets: {
 		"id": ["label@for", "input@id"],
 		"title": "label@text",
 		"value": "input@value"
 	},
+
 	actions: {
 		"valueChanged": ["input@change", "input@keyup"]
 	}
+
 });
 
 var HeaderView = View.create({
+
 	html: '<h1><span class="fahrenheit"></span>&deg; Fahrenheit equals <span class="celsius"></span>&deg; Celsius<h1>',
+
 	outlets: {
 		'c': '.celsius@text',
 		'f': '.fahrenheit@text'
 	}
+
 });
 
 // =========
 // = Model =
 // =========
 var DegreeConverterModel = new Class({
+
 	Extends: Model,
+
 	set: function(key, value){
 		this.parent(key, value.toInt() || 0);
 	},
+
 	initialize: function(celsius){
 		this.addEvent('change', function(key, value){
 			switch(key){
@@ -42,29 +52,41 @@ var DegreeConverterModel = new Class({
 		});
 		this.set('celsius', celsius);
 	}
+
 });
 
-// ================
-// = "Controller" =
-// ================
+// ==============
+// = Controller =
+// ==============
+
+var ConverterController = new Class({
+
+	Extends: Controller,
+
+	initialize: function(){
+		var myConverter = new DegreeConverterModel(0);
+
+		var fInput = new MyInputView({id: 'fahrenheit', title: 'Fahrenheit'});
+		fInput.addEvent('valueChanged', function(){
+			myConverter.set('fahrenheit', fInput.get('value'));
+		});
+		myConverter.connect('fahrenheit', fInput, 'value');
+
+		var cInput = new MyInputView({id: 'celsius', title: 'Celsius'});
+		cInput.addEvent('valueChanged', function(){
+			myConverter.set('celsius', cInput.get('value'));
+		});
+		myConverter.connect('celsius', cInput, 'value');
+
+		var header = new HeaderView({c: "0", f: "0"});
+		myConverter.connect('celsius', header, 'c');
+		myConverter.connect('fahrenheit', header, 'f');
+
+		$(document.body).adopt(header, fInput, cInput);
+	}
+
+});
+
 window.addEvent('domready', function(){
-	var myConverter = new DegreeConverterModel(0);
-
-	var fInput = new MyInputView({id: 'fahrenheit', title: 'Fahrenheit'});
-	fInput.addEvent('valueChanged', function(){
-		myConverter.set('fahrenheit', fInput.get('value'));
-	});
-	myConverter.connect('fahrenheit', fInput, 'value');
-
-	var cInput = new MyInputView({id: 'celsius', title: 'Celsius'});
-	cInput.addEvent('valueChanged', function(){
-		myConverter.set('celsius', cInput.get('value'));
-	});
-	myConverter.connect('celsius', cInput, 'value');
-
-	var header = new HeaderView({c: "0", f: "0"});
-	myConverter.connect('celsius', header, 'c');
-	myConverter.connect('fahrenheit', header, 'f');
-
-	$(document.body).adopt(header, fInput, cInput);
+	new ConverterController();
 });
---
+++ Source/Controller.js
@@ -0,0 +1,3 @@
+var Controller = new Class({
+	
+});
